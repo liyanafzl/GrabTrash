4ccf6d3fc65553bc02e8c717aaa3f198b06c9fcd
liyana <liyana.fzl@gmail.com>
2017-10-14 16:34:10 +0530

    second attempt

---
+++ app/src/main/java/college/com/grabtrash/MainActivity.java
@@ -0,0 +1,13 @@
+package college.com.grabtrash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
